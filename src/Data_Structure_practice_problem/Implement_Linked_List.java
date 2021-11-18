0e4549530f1c946010c1a5b10b28978abe4800a6
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-11-18 16:03:57 +0530

    appending the values

--- src/Data_Structure_practice_problem/Implement_Linked_List.java
+++ src/Data_Structure_practice_problem/Implement_Linked_List.java
@@ -5,9 +5,9 @@ import java.util.LinkedList;
 public class Implement_Linked_List {
 	public static void main(String[] args) {
 		Single_Linked_List list = new Single_Linked_List();
-		list.push(70);
-		list.push(30);
-		list.push(56);
+		list.append(56);
+		list.append(30);
+		list.append(70);
 		list.display();
 	}
 
