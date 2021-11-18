45834588989fd58b104e641d83233ef806e91dea
PraveenkumarElanchezhian <91645195+PraveenkumarElanchezhian@users.noreply.github.com>
2021-11-18 16:56:29 +0530

    Search the values

--- src/Data_Structure_practice_problem/Implement_Linked_List.java
+++ src/Data_Structure_practice_problem/Implement_Linked_List.java
@@ -10,10 +10,9 @@ public class Implement_Linked_List {
 		list.append(56);
 		list.append(30);
 		list.append(70);
-		list.insertBetweenValue(12, 2);
 		list.display();
 		System.out.println();
-		System.out.println("Search the element => 56,30,12,70");
+		System.out.println("Search the element => 56,30,70");
 		Scanner userInput=new Scanner(System.in);
 		int num=userInput.nextInt();
 		list.search(num);
